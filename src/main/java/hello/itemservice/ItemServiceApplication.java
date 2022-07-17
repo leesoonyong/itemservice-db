759c8c0c8dd6c42057e62387819e1a725a36ebc9
leesoonyong <44168355+leesoonyong@users.noreply.github.com>
2022-07-17 17:44:27 +0900

    레포지트로 v2로 전환

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -14,7 +14,8 @@ import javax.sql.DataSource;
 
 
 @Slf4j
-@Import(JdbcTemplateV1Config.class)
+//@Import(JdbcTemplateV1Config.class)
+@Import(JdbcTemplateV2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
